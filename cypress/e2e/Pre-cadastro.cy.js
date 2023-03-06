4376d3ccb863e637fbb5777ea4ff6e28c5af2521
regis rosa <regisrosa.silveira@hotmail.com>
2023-03-06 14:31:46 -0300

    altaração

--- cypress/e2e/Pre-cadastro.cy.js
+++ cypress/e2e/Pre-cadastro.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-var faker =require('faker');
+
 
 describe('Funcionalidade Pre cadastro', () => {
 
@@ -9,7 +9,7 @@ describe('Funcionalidade Pre cadastro', () => {
     });
 
     it('Deve completar o pre cadastro com sucesso', () => {
-       cy.get('#reg_email').type(faker.internet.email())
+       cy.get('#reg_email').type('teste39@teste.com')
        cy.get('#reg_password').type('!teste@teste$') 
        cy.get(':nth-child(4) > .button').click()
 
