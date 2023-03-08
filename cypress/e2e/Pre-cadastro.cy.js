f823965b0fdaaf9445003fb9a9e336439e45d236
regis rosa <regisrosa.silveira@hotmail.com>
2023-03-08 09:14:18 -0300

    pre cadastro completo

--- cypress/e2e/Pre-cadastro.cy.js
+++ cypress/e2e/Pre-cadastro.cy.js
@@ -8,13 +8,15 @@ describe('Funcionalidade pre cadastro',() => {
     });
 
     it('Deve completar o pre cadastro com sucesso',() => {
+    
+
         cy.get('#reg_email').type(faker.internet.email())
         cy.get('#reg_password').type('@jheni1008!')
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type('Jheni')
-        cy.get('#account_last_name').type('Monticelli')
+        cy.get('#account_first_name').type(faker.name.firstname())
+        cy.get('#account_last_name').type(faker.name.lastname())
         cy.get('.woocommerce-Button').click()
 
         cy.get('.woocommerce-message').should('contain','Detalhes da conta modificados com sucesso.')
--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -7,16 +7,29 @@ describe('Funcionalidade pagina de produtos', () => {
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get(' .product-block > .block-inner > .image > .product-image > .image-hover')
+        cy.get(' .product-block ')
             //.first()
-            //.last()
-            .eq(3)
-            .click()
+            //.click()
+            //.eq(3)
+            .contains('Arcadio Gym Short') .click()
+
     });
 
 
-    it('Deve adicionar um produto ao carrinho', () => {
-        
+    it.only('Deve adicionar um produto ao carrinho', () => {
+var quantidade= 10
+
+        cy.get(' .product-block ')
+        .contains('Arcadio Gym Short') .click()
+        cy.get('.button-variable-item-Red').click()
+        cy.get('.button-variable-item-34').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain',quantidade)
+        cy.get('.woocommerce-message').should('contain', '“Arcadio Gym Short” foram adicionados no seu carrinho.')
+            
+
     });
 
 
