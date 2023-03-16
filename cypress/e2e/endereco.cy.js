a210d07e3bfa94037df31281bb4e079762d9b42a
regis rosa <regisrosa.silveira@hotmail.com>
2023-03-16 13:02:24 -0300

    Funcao usando massa de dados

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,20 +1,43 @@
 ///<reference types="cypress"/>
 import Enderecopage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
-describe('funcionalidade endereços - faturamento e entrega', () => {
+describe('Funcionalidade endereco - Faturamento e entrega', () => {
     beforeEach(() => {
-        cy.visit('minha conta')
+        cy.visit('Minha conta');
         cy.fixture('perfil').then(dados => {
-             cy.login(dados.usuario, dados.senha)
+                cy.login(dados.usuario, dados.senha)
+           })
+   
         })
-    });
+  
 
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-Enderecopage.editarEnderecoFaturamento('Regis', 'rosa', 'google', 'Brasil', 'Rua vinicius de moraes', '752', 'Porto alegre', 'Rio grande do sul', '94930300', '08000800', 'regis@ebac.com',)
-cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso')
-    });
 
+
+    
+
+
+
+
+it('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
+    Enderecopage.editarEnderecoFaturamento(
+        dadosEndereco[1].nome,
+        dadosEndereco[1].Sobrenome,
+        dadosEndereco[1].Empresa,
+        dadosEndereco[1].pais,
+        dadosEndereco[1].Endereco,
+        dadosEndereco[1].Numero,
+        dadosEndereco[1].cidade,
+        dadosEndereco[1].Estado,
+        dadosEndereco[1].cep,
+        dadosEndereco[1].telefone,
+        dadosEndereco[1].email
+    )
 });
+})
+
+
+
 
 
 
